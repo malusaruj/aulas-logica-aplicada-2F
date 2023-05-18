aee529ec2d059fc669728a8c3667bc4d47c1767a
malusarauj <maria.luizasa06@gmail.com>
2023-05-18 08:26:51 -0300

    Pessoa: classe; eva: objeto; new Pessoa(): construtor;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Aula de Orientação à Objetos
+        // Declarar objetos
+
+        Pessoa adao;
+
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Pessoa: classe; eva: objeto; new Pessoa(): construtor;
+
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
     }
 }
